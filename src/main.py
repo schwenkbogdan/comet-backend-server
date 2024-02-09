d650e76cdd64fc43d479a926f510e8bca82869b2
Debian virtual <no name>
2024-02-09 11:01:05 +0700

    Initial commit

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
 # Создание экземпляра приложения FastAPI app = FastAPI()
 
 # Настройки подключения к базе данных
-DATABASE_URL = "postgresql://postgres:@localhost/cometdb"
+DATABASE_URL = "postgresql://postgres:pass@localhost/cometdb"
 
 # Инициализация базы данных
 database = Database(DATABASE_URL)
